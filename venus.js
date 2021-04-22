70bc6c667af873c9ffec7b142e8675d3e106e8dc
jiayisaidz <82979922+jiayisaidz@users.noreply.github.com>
2021-04-22 20:15:18 +0700

    Update venus.js

--- venus.js
+++ venus.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "https://mars.saidz.my.id/venus.html?url=" + encryptedUrl;
+a_to_ve[i].href = "https://mars.saidz.my.id/venus.html?f=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
